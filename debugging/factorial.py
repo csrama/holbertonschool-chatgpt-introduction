32819a4c8b4308e6b40f946a79c1e9aaad91a9fa
csrama <ramadafer1@gmail.com>
2026-01-12 06:57:48 +0000

    The problem is that n never changes inside the loop, so if n > 1, the loop will run forever

---
+++ debugging/factorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  /the fix to the code/
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
